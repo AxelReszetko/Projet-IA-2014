134cb72477cc195dd2010438993f6f807bf33f5f
Pierre Pézot <pierre.pezot@telecomnancy.net>
2014-02-05 18:21:57 +0100

    astar implemented (doesn't work yet)

--- pacman/src/fr/univ_lorraine/pacman/controller/ghost/GhostBetterStrategy.java
+++ pacman/src/fr/univ_lorraine/pacman/controller/ghost/GhostBetterStrategy.java
@@ -1,10 +1,13 @@
 package fr.univ_lorraine.pacman.controller.ghost;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import com.badlogic.gdx.math.Vector2;
 
 import fr.univ_lorraine.pacman.controller.GhostController;
+import fr.univ_lorraine.pacman.model.Block;
+import fr.univ_lorraine.pacman.model.GameBasicElement;
 import fr.univ_lorraine.pacman.model.Ghost;
 import fr.univ_lorraine.pacman.model.Pacman;
 import fr.univ_lorraine.pacman.model.World;
@@ -131,7 +134,21 @@ public class GhostBetterStrategy extends GhostController{
 		while(iterGhost.hasNext())
 		{
 			Ghost gh = iterGhost.next();
-			Vector2 pacPosition = pac.getPosition();
+			Node ghPosition = new Node((int)Math.floor(gh.getPosition().x),(int)Math.floor(gh.getPosition().y));
+			Node goal = new Node(13,19);
+			ArrayList<Direction> direction; 
+			direction = astar(ghPosition, goal, new ArrayList<Direction>());
+			for(int i = 0; i<direction.size();++i)
+			{
+				System.out.println(direction.get(i).toString());
+			}
+			try {
+				Thread.sleep(60000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		/*	Vector2 pacPosition = pac.getPosition();
 			Vector2 ghPosition = gh.getPosition();
 			float deltaX = pacPosition.x - ghPosition.x;
 			float deltaY = pacPosition.y - ghPosition.y;
@@ -141,6 +158,9 @@ public class GhostBetterStrategy extends GhostController{
 			
 			if(ghY<19 && ghY>10 && ghX<9 && ghX>13)
 			{
+				if(goal.equals(null))
+					goal=new Node (13,19);
+				
 				
 			}
 			
@@ -157,12 +177,86 @@ public class GhostBetterStrategy extends GhostController{
 					gh.turnRight();
 				else
 					gh.turnLeft();
-			}	
+			}	*/
 			gh.update(delta);
 		}
 		
 	}
 	
+	private ArrayList<Direction> astar(Node from, Node to, ArrayList<Direction> path)
+	{
+		if(!from.equals(to))
+		{
+			ArrayList<Node> open = new ArrayList<>();
+			ArrayList<Node> close = new ArrayList<>();
+			open.add(from);
+			int localX = from.getX();
+			int localY = from.getY();
+			if(!(world.getElement(localX, localY - 1) instanceof Block))//look down
+			{
+				Node n = new Node(localX , localY - 1);
+				n.setComeFrom(from);
+				n.setDist(manhattan(n, to));
+				open.add(n);
+			}
+			if(!(world.getElement(localX, localY + 1) instanceof Block))//look up
+			{
+				Node n = new Node(localX, localY + 1);
+				n.setComeFrom(from);
+				n.setDist(manhattan(n, to));
+				open.add(n);
+			}
+			if(!(world.getElement(localX - 1, localY) instanceof Block))//look left
+			{
+				Node n = new Node(localX - 1, localY);
+				n.setComeFrom(from);
+				n.setDist(manhattan(n, to));
+				open.add(n);
+			}
+			if(!(world.getElement(localX + 1, localY) instanceof Block))//look right
+			{
+				Node n = new Node(localX + 1, localY);
+				n.setComeFrom(from);
+				n.setDist(manhattan(n, to));
+				open.add(n);
+			}
+
+			close.add(open.get(0));
+			open.remove(0);
+			Node lowest = open.get(0);
+			int i = 1;
+			for (i =1; i<open.size();++i)
+			{
+				if(open.get(i).dist < lowest.dist)
+					lowest = open.get(i);
+			}
+			int dir = -1;
+			if(lowest.x < lowest.getComeFrom().x)
+				dir = 3;
+			if(lowest.x > lowest.getComeFrom().x)
+				dir = 2;
+			if(lowest.y < lowest.getComeFrom().y)
+				dir = 1;
+			if(lowest.y > lowest.getComeFrom().y)
+				dir = 0;
+			path.add(new Direction(lowest.x, lowest.y, dir));
+			astar(lowest, to, path);
+			return null;
+		}
+		else
+		{
+			return path;
+		}
+		
+	}
+	
+	private int manhattan(Node from, Node to)
+	{
+		return Math.abs(from.x-to.x)+Math.abs(from.y-to.y);
+	}
+	
 	private Pacman pac = world.getPacman();
+	private ArrayList<Direction> ghPath = new ArrayList<>();
+	private Node goal;
 
 }
